9b8849eb578cb8e3e1de503767eab15e9bfb7f91
shenlanAZ <shenlanluck@gmail.com>
2021-12-03 09:09:12 +0800

    fix user loader bug.

--- user/src/main/java/ltd/inmind/user/configuration/UserLoader.java
+++ user/src/main/java/ltd/inmind/user/configuration/UserLoader.java
@@ -10,6 +10,8 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -25,16 +27,22 @@ public class UserLoader {
             Toml toml = new Toml()
                     .read(resource.getInputStream());
 
-            final List<User> list = toml.getTable("user")
-                    .getList("list", new ArrayList<>());
+            final List<User> list = toml.getList("users")
+                    .stream().map(ele -> toUser((Map) ele))
+                    .collect(Collectors.toList());
 
             userList.addAll(list);
-        } catch (IOException e) {
+        } catch (IOException | RuntimeException e) {
             log.error("read user failed, switch to default data.");
             userList.clear();
             userList.add(new User("default", "default@inmind.ltd"));
         }
     }
 
+
+    User toUser(Map ele) {
+
+        return new User((String) ele.get("name"), (String) ele.get("account"));
+    }
 
 }
