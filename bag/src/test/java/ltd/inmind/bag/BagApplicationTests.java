66b4fe3dbe8e2d63c309078593ae5cae40c8b95e
shenlan <shenlanluck@gmail.com>
2021-12-07 21:09:22 +0800

    fix spring boot test.

--- bag/src/test/java/ltd/inmind/bag/BagApplicationTests.java
+++ bag/src/test/java/ltd/inmind/bag/BagApplicationTests.java
@@ -1,9 +1,14 @@
 package ltd.inmind.bag;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariables;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfEnvironmentVariables({
+        @EnabledIfEnvironmentVariable(named = "api.stock.service", matches = "api.stock.service")
+})
 class BagApplicationTests {
 
     @Test
